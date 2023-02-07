574cb5a956cc7afbb645b235e80cad668ba9558a
Abhijaan Ganguly <63783904+RUSHES02@users.noreply.github.com>
2023-02-07 14:21:04 +0530

    update b to 25

--- addjava.java
+++ addjava.java
@@ -1,6 +1,8 @@
 class test{
     public static void main(String[] args) {
+    int a = 20;
+    int b = 25;
         System.out.println("Hello World");
-         System.out.println("Hello World");
+         System.out.println(a + " " + b);
     }
 }
