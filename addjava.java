83faf4eabd8425b8c18d666993efceeab22435e4
NEUROTICkid <alfiebpl85@gmail.com>
2023-02-07 14:11:29 +0530

    add day

--- addjava.java
+++ addjava.java
@@ -1,6 +1,8 @@
 class test{
     public static void main(String[] args) {
+    int a = 20;
+    int b = 20;
         System.out.println("Hello World");
-         System.out.println("Hello World");
+         System.out.println(a + " " + b);
     }
 }
