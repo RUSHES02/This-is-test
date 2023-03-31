1d89d4d759de746ea8cd1e91ed72c9e1734d4fa3
shouvikde22 <shouvikde7@gmail.com>
2023-03-31 21:28:37 +0530

    change values

--- addjava.java
+++ addjava.java
@@ -1,7 +1,7 @@
 class test{
     public static void main(String[] args) {
-    int a = 20;
-    int b = 30;
+    int a = 30;
+    int b = 20;
         System.out.println("Hello World");
          System.out.println(a + " " + b);
     }
